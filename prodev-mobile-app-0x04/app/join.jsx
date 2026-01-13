399fc177f63e4a1baa37c939c65aaea2e515952d
OgborFaith <faithogbor23@gmail.com>
2026-01-13 15:36:31 +0100

    Implement stack navigation with index, join, and signin screens

---
+++ prodev-mobile-app-0x04/app/join.jsx
@@ -0,0 +1,44 @@
+import { View, Text, TextInput, Pressable } from "react-native";
+import { styles } from "@/styles/_joinstyle";
+import { useRouter } from "expo-router";
+
+export default function Join() {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleTextGroup}>
+        <Text style={styles.titleText}>Create Account</Text>
+        <Text style={styles.subText}>Let’s get you started</Text>
+      </View>
+
+      <View style={styles.formGroup}>
+        <View>
+          <Text style={styles.formLabel}>Full Name</Text>
+          <TextInput style={styles.formControl} />
+        </View>
+
+        <View>
+          <Text style={styles.formLabel}>Email</Text>
+          <TextInput style={styles.formControl} />
+        </View>
+
+        <View>
+          <Text style={styles.formLabel}>Password</Text>
+          <TextInput style={styles.formControl} secureTextEntry />
+        </View>
+      </View>
+
+      <Pressable style={styles.primaryButton}>
+        <Text style={styles.buttonText}>Create Account</Text>
+      </Pressable>
+
+      <View style={styles.signupgroup}>
+        <Text style={styles.signupTitleText}>Already have an account? </Text>
+        <Pressable onPress={() => router.push("/signin")}>
+          <Text style={styles.signupSubTitleText}>Sign in</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
